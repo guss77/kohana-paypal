06f8036f014d1df024aaa6afea5dff0a2fb5691f
Oded Arbel <oded@geek.co.il>
2014-03-08 14:34:37 +0200

    added copyright and license

--- classes/Controller/PayPal.php
+++ classes/Controller/PayPal.php
@@ -1,3 +1,3 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_PayPal extends Kohana_Controller_PayPal {}
--- classes/Kohana/PayPal.php
+++ classes/Kohana/PayPal.php
@@ -1,6 +1,28 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+/*
+ * Kohana 3.3 PayPal payment processing Module
+ * Copyright (C) 2014 Oded Arbel
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+/**
+ * PayPal API implementation
+ * To process payments, create a new instance of this class and call the {@link Kohana_PayPal#payment}
+ * method.
+ * @author Oded Arbel <oded@geek.co.il>
+ */
 class Kohana_PayPal {
 
 	const CACHE_TOKEN = 'paypal_cached_token';
